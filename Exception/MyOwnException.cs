5261765f9b64ab4c287a9f12cd7e837289414726
user3107 <0alexchebanu@gmail.com>
2022-08-19 16:43:04 +0300

    initial

---
+++ Exception/MyOwnException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exception_
+{
+    class MyOwnException : Exception
+    {
+        public MyOwnException() : base("This exception is mine\nYou entered wrong data.\nTry again")
+        {
+             
+        }
+
+        public MyOwnException(string message) : base(message)
+            {
+
+            }
+    }
+}
---
+++ Exception/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Exception_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] PhoneBook = 
+                { 
+                "Alexei Chebanu - 380636188311", 
+                "Interesnoe Syschestvo - 380669580534"
+            };
+
+            List<string> result = new List<string>();
+            Console.Write("Type the user you are looking for: ");
+
+            string answer = Console.ReadLine();
+            var regex = new Regex(answer);
+
+            try
+            {
+                
+                foreach (var contact in PhoneBook)
+                {
+                    MatchCollection match = regex.Matches(contact);
+                    int counter = match.Count;
+                    if (match.Count > 0)
+                        result.Add(contact);
+                }
+                if (result.Count > 0)
+                {
+                    Console.WriteLine("Matches:");
+                    foreach (var foundUser in result)
+                        Console.WriteLine(foundUser);
+                }
+                else
+                {
+                    /*for instance. Type "f" to throw the exception*/
+                    throw new MyOwnException();
+                }
+            }
+            catch(MyOwnException ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+        }
+    }
+}
